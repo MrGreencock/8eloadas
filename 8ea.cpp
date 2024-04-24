fc1167381088fb0e59cd42b37ca463b8cda8b595
Ferenczi Benjámin <66739561+Ptolemajosz@users.noreply.github.com>
2024-04-24 14:45:56 +0200

    Update and rename 8ea to 8ea.cpp

---
+++ 8ea.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main() {
+   cout << "Adj meg egy számot << endl;
+   int a;
+   cin >> a;
+   for(int i=0; i < 5; i++) {
+       cout << "Adj meg egy számot << endl;
+       int a;
+       cin >> a;
+    }
+}
